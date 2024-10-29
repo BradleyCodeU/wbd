e3d440c6c8b4edf70682a376dc8ecb8269c4be1c
Justin Riley <20hz20khz@gmail.com>
2024-10-29 07:04:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 /*global loop,colorMode,RGB,HSB,int,abs,angleMode,append,background,beginShape,bezier,box,camera,ceil,CENTER,color,cone,cos,createCanvas,createCanvas,createGraphics,curveVertex,cylinder,DEGREES,displayHeight,displayWidth,dist,div,DOWN_ARROW,ellipse,endShape,fill,floor,frameCount,frameRate,height,image,key,keyCode,keyIsDown,keyIsPressed,keyIsPressed,keyPressed,LEFT,LEFT_ARROW,lerpColor,line,loadImage,loadJSON,loadSound,map,mouseIsPressed,mouseX,mouseY,noFill,noLoop,normalMaterial,noStroke,p5,plane,point,pointLight,pop,push,push,RADIANS,radians,random,rect,resizeCanvas,resizeCanvas,RIGHT,RIGHT_ARROW,rotate,rotateX,rotateY,rotateZ,round,round,scale,shuffle,sin,sphere,stroke,strokeWeight,text,textAlign,textFont,textSize,texture,textWidth,torus,translate,triangle,UP_ARROW,WEBGL,width,windowHeight,windowHeight,windowWidth,world */
 
 let dots = [];
-let selectBoxStartPos;
-let selectBoxEndPos;
-let isDrawingSelectBox = true;
+let selectBoxStartPos,selectBoxEndPos;
+let selectBoxClicks = 0;
 //let length = 0;
 const colorGet = document.getElementById("colorPicker");
 const pencilButton = document.getElementById("Pencil");
@@ -17,12 +16,13 @@ const fontsizeRange = document.getElementById("tSize");
 const EsizeRange = document.getElementById("eSize");
 const confettiCheckbox = document.getElementById("confetti");
 const wiggleCheckbox = document.getElementById("wiggle");
-let dotColor;
+
 let penSize;
 function setup() {
   let myCanvas = createCanvas(windowWidth, windowHeight);
   myCanvas.parent("canvasDiv");
   colorMode(RGB, 100);
+  dots.push(new Dot(110,150,150,110,setDotColor(),3))
 }
 
 function drawEraser(minimumDistance) {
@@ -49,11 +49,25 @@ function drawEraser(minimumDistance) {
     15
   );
   pop();
+
+}
+
+function drawTheSelectBox() {
+  push()
+  noFill();
+  stroke(cos(frameCount * .5) * 10 + 80, sin(frameCount * .1) * 10 + 90, sin(frameCount * .5) * 50 + 90)
+  if(selectBoxClicks == 1){
+    rect(selectBoxStartPos.x, selectBoxStartPos.y, mouseX - selectBoxStartPos.x, mouseY - selectBoxStartPos.y);
+  }
+  else if(selectBoxClicks == 2){
+    rect(selectBoxStartPos.x, selectBoxStartPos.y, selectBoxEndPos.x-selectBoxStartPos.x, selectBoxEndPos.y-selectBoxStartPos.y);
+  }
+  pop()
 }
 
 function addText() {
   //textButton.checked = false;
-  setDotColor();
+  
   let text = prompt();
   let fontSize = parseInt(fontsizeRange.value);
   dots[dots.length] = new Word(
@@ -61,7 +75,7 @@ function addText() {
     mouseY,
     null,
     null,
-    dotColor,
+    setDotColor(),
     fontSize,
     text
   );
@@ -79,101 +93,102 @@ function editWord(someWord) {
   //dots.splice(-1);
 }
 
-function changeDots() {
-  if (mouseY < 0) {
-    return;
-  }
-
-  // PENCIL
-  if (mouseIsPressed && pencilButton.checked == true) {
-    setDotColor();
-    penSize = parseInt(sizeRange.value);
-    dots[dots.length] = new Dot(
-      mouseX,
-      mouseY,
-      pmouseX,
-      pmouseY,
-      dotColor,
-      penSize
-    );
-    //length++;
-
-    // ERASER
-  } else if (mouseIsPressed && eraserButton.checked == true) {
-    let minimumDistance = parseInt(EsizeRange.value);
-    //alert(typeof minimumDistance)
-    drawEraser(minimumDistance);
-    for (let i = 0; i < dots.length; i++) {
-      if (dots[i].getDistance(mouseX, mouseY) < minimumDistance) {
-        dots[i].color = "#ffffff";
-        dots[i].show();
-        //length--;
-        dots.splice(i, 1);
-      }
-    }
-    // SPRAY
-  } else if (mouseIsPressed && sprayButton.checked == true) {
-    setDotColor();
-    let sprayScale = 1.5;
-    penSize = parseInt(sizeRange.value) * 0.5;
-    dots[dots.length] = new Dot(mouseX, mouseY, null, null, dotColor, penSize);
-    //length++;
-    setDotColor();
-    dots[dots.length] = new Dot(
-      mouseX + int(random(0, penSize * sprayScale)),
-      mouseY + int(random(-penSize * sprayScale, penSize * sprayScale)),
-      null,
-      null,
-      dotColor,
-      penSize
-    );
-    //length++;
-    setDotColor();
-    dots[dots.length] = new Dot(
-      mouseX - int(random(0, penSize * sprayScale)),
-      mouseY + int(random(-penSize * sprayScale, penSize * sprayScale)),
-      null,
-      null,
-      dotColor,
-      penSize
-    );
-    //length++;
-    setDotColor();
-    dots[dots.length] = new Dot(
-      mouseX + int(random(-penSize * sprayScale, penSize * sprayScale)),
-      mouseY + int(random(0, penSize * sprayScale)),
-      null,
-      null,
-      dotColor,
-      penSize
-    );
-    //length++;
-    setDotColor();
-    dots[dots.length] = new Dot(
-      mouseX + int(random(-penSize * sprayScale, penSize * sprayScale)),
-      mouseY - int(random(0, penSize * sprayScale)),
-      null,
-      null,
-      dotColor,
-      penSize
-    );
-    //length++;
-
-  }
-
+function makeDotsWithPencil() {
+  
+  penSize = parseInt(sizeRange.value);
+  dots[dots.length] = new Dot(
+    mouseX,
+    mouseY,
+    pmouseX,
+    pmouseY,
+    setDotColor(),
+    penSize
+  );
+  //length++;
 }
+function makeDotsWithSpray() {
+  
+  let sprayScale = 1.5;
+  penSize = parseInt(sizeRange.value) * 0.5;
+  dots[dots.length] = new Dot(mouseX, mouseY, null, null, setDotColor(), penSize);
+  //length++;
+  
+  dots[dots.length] = new Dot(
+    mouseX + int(random(0, penSize * sprayScale)),
+    mouseY + int(random(-penSize * sprayScale, penSize * sprayScale)),
+    null,
+    null,
+    setDotColor(),
+    penSize
+  );
+  //length++;
+  
+  dots[dots.length] = new Dot(
+    mouseX - int(random(0, penSize * sprayScale)),
+    mouseY + int(random(-penSize * sprayScale, penSize * sprayScale)),
+    null,
+    null,
+    setDotColor(),
+    penSize
+  );
+  //length++;
+  
+  dots[dots.length] = new Dot(
+    mouseX + int(random(-penSize * sprayScale, penSize * sprayScale)),
+    mouseY + int(random(0, penSize * sprayScale)),
+    null,
+    null,
+    setDotColor(),
+    penSize
+  );
+  //length++;
+  
+  dots[dots.length] = new Dot(
+    mouseX + int(random(-penSize * sprayScale, penSize * sprayScale)),
+    mouseY - int(random(0, penSize * sprayScale)),
+    null,
+    null,
+    setDotColor(),
+    penSize
+  );
+  //length++;
+}
+function removeDotsWithEraser() {
+  let minimumDistance = parseInt(EsizeRange.value);
+  
+  for (let i = 0; i < dots.length; i++) {
+    if (dots[i].getDistance(mouseX, mouseY) < minimumDistance) {
+      dots[i].color = "#ffffff";
+      dots[i].show();
+      //length--;
+      dots.splice(i, 1);
+    }
+  }
+}
+
+
 
 
 
 function mousePressed() {
   //alert()
-  setDotColor();
-  penSize = parseInt(sizeRange.value);
-  changeDots()
-  //dots[dots.length] = new Dot(mouseX, mouseY, null, null, dotColor, penSize);
-  //length++;
   
-  // ENABLE MOVE
+  penSize = parseInt(sizeRange.value);
+  // PENCIL
+  if (pencilButton.checked == true && mouseY > 1) {
+    makeDotsWithPencil()
+    // ERASER
+  } else if (eraserButton.checked == true) {
+    removeDotsWithEraser()
+    // SPRAY
+  } else if (sprayButton.checked == true && mouseY > 1) {
+    makeDotsWithSpray()
+
+  }
+  //dots[dots.length] = new Dot(mouseX, mouseY, null, null, setDotColor(), penSize);
+  //length++;
+
+  // ENABLE MOVE WITH PINCH
   if (mouseIsPressed && moveButton.checked == true) {
     let minimumDistance = parseInt(EsizeRange.value);
     for (let i = 0; i < dots.length; i++) {
@@ -195,98 +210,168 @@ function mousePressed() {
     }
     addText();
     //length++;
-  // SELECT BOX
-  } else if (mouseIsPressed && selectBoxButton.checked == true && isDrawingSelectBox && mouseY > 15) {
-    
-    startPos = createVector(mouseX, mouseY);
+    // SELECT BOX DRAW
+  } else if (mouseIsPressed && selectBoxButton.checked == true && selectBoxClicks == 0 && mouseY > 15) {
+    selectBoxClicks = 1;
+    console.log(selectBoxClicks)
+    selectBoxStartPos = createVector(mouseX, mouseY);
+  // SELECT BOX MOVE
+  } else if (mouseIsPressed && selectBoxButton.checked == true && selectBoxClicks == 2 && mouseY > 15) {
+    selectBoxClicks = 3;
+    // is dot within select box???
+      for (let i = 0; i < dots.length; i++) {
+        
+        if (
+          selectBoxStartPos.x <= dots[i].x &&
+          selectBoxEndPos.x >= dots[i].x &&
+          selectBoxStartPos.y <= dots[i].y &&
+          selectBoxEndPos.y >= dots[i].y
+        ) {
+          dots[i].isGrabbed = true;
+
+        }
+      }
+   
   }
 
   loop();
+
+
 }
 
-  function mouseDragged() {
-    if (mouseIsPressed && moveButton.checked == true) {
-      for (let i = 0; i < dots.length; i++) {
-        if (dots[i].isGrabbed) {
-          dots[i].moveTo(mouseX, mouseY)
-        }
+function mouseDragged() {
+  // PENCIL
+  if (pencilButton.checked == true && mouseY > 1) {
+    makeDotsWithPencil()
+    // ERASER
+  } else if (eraserButton.checked == true) {
+    removeDotsWithEraser()
+    // SPRAY
+  } else if (sprayButton.checked == true && mouseY > 1) {
+    makeDotsWithSpray()
 
-      }
-    } 
-    
   }
-
-  function mouseReleased() {
-    if (moveButton.checked == true) {
-      for (let i = 0; i < dots.length; i++) {
-        dots[i].isGrabbed = false;
-      }
-    }
-  }
-
-  function draw() {
-    //alert("drawy")
-    if (!mouseIsPressed && !wiggleCheckbox.value) {
-      noLoop();
-    }
-
-    background(245);
-    // select box
-    if (mouseIsPressed && selectBoxButton.checked == true && isDrawingSelectBox && mouseY > 15) {
-      //noLoop();
-      push()
-      noFill();
-      stroke(sin(frameCount*7)*50+70,sin(frameCount*9)*50+70,sin(frameCount*8)*50+70)
-      rect(startPos.x, startPos.y, mouseX -startPos.x, mouseY-startPos.y);
-      pop()
-    }
-    else if (mouseIsPressed) {
-      changeDots();
-    }
-    if (wiggleCheckbox.value) {
-      doWiggle();
-    }
-
-
+  // PINCH MOVE TOOL
+  else if (mouseIsPressed && moveButton.checked == true) {
     for (let i = 0; i < dots.length; i++) {
-      dots[i].show();
-    }
-  }
-
-  function clearAll() {
-    for (let i = 0; i < dots.length; i++) {
-      dots[i].color = "#ffffff";
-      dots[i].show();
-    }
-    dots = [];
-    dots.length = 0;
-  }
-
-  function setDotColor() {
-    if (confettiCheckbox.checked) {
-      colorMode(HSB, 100);
-      let h = int(random(0, 100));
-      let s = int(random(90, 100));
-      let b = int(random(90, 100));
-
-      dotColor = color(h, s, b);
-    } else {
-      colorMode(RGB, 100);
-      dotColor = color(colorGet.value);
-    }
-  }
-
-  function doWiggle() {
-    if (wiggleCheckbox.checked) {
-      for (let i = 0; i < dots.length; i++) {
-        dots[i].wiggle();
+      if (dots[i].isGrabbed) {
+        dots[i].moveTo(mouseX, mouseY)
       }
+
+    }
+  }// SELECT BOX MOVE TOOL
+  else if (mouseIsPressed && selectBoxButton.checked == true && selectBoxClicks == 3) {
+    for (let i = 0; i < dots.length; i++) {
+      if (dots[i].isGrabbed) {
+        dots[i].moveTo(mouseX + dots[i].x - pmouseX, mouseY + dots[i].y - pmouseY)
+      }
+
+    }
+    console.log(dots)
+  }
+  loop();
+  
+}
+
+
+
+function mouseReleased() {
+  if (moveButton.checked == true) {
+    for (let i = 0; i < dots.length; i++) {
+      dots[i].isGrabbed = false;
     }
   }
-
-  function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+  if (selectBoxButton.checked == true && selectBoxClicks == 1){
+    selectBoxEndPos = createVector(mouseX, mouseY);
+    selectBoxClicks = 2;
+    console.log(selectBoxClicks)
+  } else if(selectBoxClicks == 3){
+    selectBoxClicks = 0;
+    for (let i = 0; i < dots.length; i++) {
+      dots[i].isGrabbed = false;
+    }
   }
+}
+
+function draw() {
+  //alert("drawy")
+  if (!mouseIsPressed && !wiggleCheckbox.value) {
+    noLoop();
+  }
+
+  background(245);
+  // select box
+  if (selectBoxButton.checked == true && selectBoxClicks > 0 && mouseY > 15) {
+    //noLoop();
+    drawTheSelectBox()
+  }
+
+  if (wiggleCheckbox.value) {
+    doWiggle();
+  }
+
+
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].show();
+  }
+  if (eraserButton.checked == true) {
+    let minimumDistance = parseInt(EsizeRange.value);
+
+    drawEraser(minimumDistance);
+  }
+}
+
+function clearAll() {
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].color = "#ffffff";
+    dots[i].show();
+  }
+  dots = [];
+  dots.length = 0;
+}
+
+function zoomIn(){
+  let midpoint = createVector(width/2,height/2)
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].moveTo(midpoint.x + (dots[i].x - midpoint.x) * 1.1, midpoint.y + (dots[i].y - midpoint.y) * 1.1)
+    dots[i].grow()
+  }
+  
+}
+function zoomOut(){
+  let midpoint = createVector(width/2,height/2)
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].moveTo(midpoint.x + (dots[i].x - midpoint.x) * 0.9, midpoint.y + (dots[i].y - midpoint.y) * 0.9)
+    dots[i].shrink()
+  }
+  
+}
+
+function setDotColor() {
+  if (confettiCheckbox.checked) {
+    colorMode(HSB, 100);
+    let h = int(random(0, 100));
+    let s = int(random(90, 100));
+    let b = int(random(90, 100));
+
+    return color(h, s, b);
+  } else {
+    colorMode(RGB, 100);
+    return color(colorGet.value);
+  }
+}
+
+function doWiggle() {
+  if (wiggleCheckbox.checked) {
+    for (let i = 0; i < dots.length; i++) {
+      dots[i].wiggle();
+    }
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
 
 
 
